454cb8b3af3b7f6be8869916e0741795052a6e8f
Sherif Nassif <sherif.nassif@ericsson.com>
2024-03-24 20:31:14 +0000

    using vars

--- europe/auto-scale/main.tf
+++ europe/auto-scale/main.tf
@@ -1,6 +1,8 @@
 module "terrafrom_ASG" {
   source  = "git::ssh://git@ssh.dev.azure.com/v3/ericsson/Sapper/Terraform_Modules_AWS//autoscaling-module"
   port    = "80"
+  image = "ami-00381a880aa48c6c6" #Ubuntu Server 22.04 LT x86 64 bits
+  flavor = "t3.micro"
 }
 
 
